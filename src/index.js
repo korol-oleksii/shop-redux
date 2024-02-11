24dc88123bb7a6bb2b56cd83d6eb85a404b30b11
JUKorol <kaleksey1982@gmail.com>
2024-02-11 20:50:57 +0200

    init ph-pages and add basename

--- src/index.js
+++ src/index.js
@@ -8,7 +8,7 @@ import store from "./data/store/store";
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-    <BrowserRouter>
+    <BrowserRouter basename='/shop-redux'>
         <Provider store={store}>
             <App />
         </Provider>
