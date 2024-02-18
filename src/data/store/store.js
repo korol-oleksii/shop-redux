5818b38173624d9706f426e4ea6ca7933a7e10ee
JUKorol <kaleksey1982@gmail.com>
2024-02-18 19:50:49 +0200

    save productsCart and reviews to LocalStorage

--- src/data/store/store.js
+++ src/data/store/store.js
@@ -1,12 +1,41 @@
-import {configureStore} from "@reduxjs/toolkit";
+import {configureStore, combineReducers} from "@reduxjs/toolkit";
+//for localStorage
+import {
+    persistStore,
+    persistReducer,
+    FLUSH,
+    REHYDRATE,
+    PAUSE,
+    PERSIST,
+    PURGE,
+    REGISTER,
+} from 'redux-persist';
+import storage from 'redux-persist/lib/storage';
+//reducers
 import shopReducer from "../reducers/shopReducer";
 import shoppingCartReducer from "../reducers/shoppingCartReducer";
 
+const rootReducer = combineReducers({
+    shop: shopReducer,
+    shoppingCart: shoppingCartReducer,
+});
+
+const persistConfig = {
+    key: 'root',
+    storage,
+}
+
+const persistedReducer = persistReducer(persistConfig, rootReducer);
+
 let store = configureStore({
-    reducer: {
-        shop: shopReducer,
-        shoppingCart: shoppingCartReducer,
-    }
+    reducer: persistedReducer,
+    middleware: (getDefaultMiddleware) =>
+        getDefaultMiddleware({
+            serializableCheck: {
+                ignoredActions: [FLUSH, REHYDRATE, PAUSE, PERSIST, PURGE, REGISTER],
+            },
+        }),
 })
 
+export const persistor = persistStore(store);
 export default store;
--- src/index.js
+++ src/index.js
@@ -4,13 +4,16 @@ import './index.css';
 import App from './App';
 import {BrowserRouter} from "react-router-dom";
 import {Provider} from "react-redux";
-import store from "./data/store/store";
+import {PersistGate} from "redux-persist/integration/react";
+import store, {persistor} from "./data/store/store";
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
     <BrowserRouter basename='/shop-redux'>
         <Provider store={store}>
-            <App />
+            <PersistGate loading={null} persistor={persistor}>
+                <App />
+            </PersistGate>
         </Provider>
     </BrowserRouter>
 );
